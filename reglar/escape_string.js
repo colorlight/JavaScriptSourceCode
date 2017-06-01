0cba34d93d1b58578c184109783785019bb1ce72
colorlight <colorlightleecaa@gmail.com>
2017-06-01 14:03:13 +0800

    add escape_string.h

---
+++ reglar/escape_string.js
@@ -0,0 +1,13 @@
+function html_encode(str)   
+{   
+  var s = "";   
+  if (str.length == 0) return "";   
+  s = str.replace(/&/g, "&gt;");   
+  s = s.replace(/</g, "&lt;");   
+  s = s.replace(/>/g, "&gt;");   
+  s = s.replace(/ /g, "&nbsp;");   
+  s = s.replace(/\'/g, "&#39;");   
+  s = s.replace(/\"/g, "&quot;");   
+  s = s.replace(/\n/g, "<br>");   
+  return s;   
+}   
